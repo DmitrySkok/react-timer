0cd9c7d6229d1efaa6ec11fd7e09837550413fd1
Dmitry Skok <dimas.skok95@gmail.com>
2022-02-27 20:59:32 +0100

    Add func formatTime

--- src/App.js
+++ src/App.js
@@ -18,14 +18,17 @@ const App  = () => {
     return () => clearInterval(interval)
   }, [start]);
 
+  const formatTime = time => {
+    const hours = ("0" + Math.floor((time / 36000000) % 24)).slice(-2);
+    const minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+    const seconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+    const mileSeconds = Math.floor(time % 1000);
+    return `${hours}:${minutes}:${seconds}:${mileSeconds}`
+  }
+
   return (
     <div className="App">
-      <h1>
-        <span>{("0" + Math.floor((time / 36000000) % 24)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{Math.floor((time) % 1000)}</span>
-      </h1>
+      <h1>{formatTime(time)}</h1>
       <Button action={() => setStart(true)}>Start</Button>
       <Button action={() => setStart(false)}>Pause</Button>
       <Button action={() => {setTime(0); setStart(false);}}>Stop</Button>
