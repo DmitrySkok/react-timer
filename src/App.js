6a478d94e62c0587d2248ed1553a115ceb6ad051
Dmitry Skok <dimas.skok95@gmail.com>
2022-02-27 15:47:08 +0100

    complete timer(need add formate time func)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import button from './Components/button/button';
+import { useEffect, useState } from 'react';
+import Button from './Components/Button/Button';
 
-function App() {
+const App  = () => {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(start) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval)
+  }, [start]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <span>{("0" + Math.floor((time / 36000000) % 24)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{Math.floor((time) % 1000)}</span>
+      </h1>
+      {/* HOW USE BUTTON */}
+      <Button action={() => setStart(true)}>Start</Button>
+      <Button action={() => setStart(false)}>Pause</Button>
+      <Button action={() => {setTime(0); setStart(false);}}>Stop</Button>
     </div>
   );
 }
